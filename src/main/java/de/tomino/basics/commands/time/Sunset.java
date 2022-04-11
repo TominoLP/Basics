4bdf67c0fb3df46ccac51e9160ce4aa52d1a117d
TominoLP <erfinder.tom@gmail.com>
2022-04-11 15:46:21 +0200

    added Sunset time

---
+++ src/main/java/de/tomino/basics/commands/time/Sunset.java
@@ -0,0 +1,40 @@
+package de.tomino.basics.commands.time;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Sunset implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Player executor = (Player) sender;
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.sendMessage("accelerating threw the night");
+
+        }
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("Basics"), new Runnable() {
+
+            @Override
+            public void run() {
+                if (executor.getWorld().getTime() > 11900 && executor.getWorld().getTime() < 12300) {
+
+                    Bukkit.getScheduler().cancelTasks(Bukkit.getPluginManager().getPlugin("Basics"));
+                } else {
+                    executor.getWorld().setTime((int) executor.getWorld().getTime() + 300);
+                }
+            }
+        }, 0L, 1L);
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.sendMessage("Sunset...");
+
+        }
+
+
+        return false;
+    }
+}
